2eb4330b6e338974e059c74c45825091a7835b9f
Dustin Van Tate Testa <toast27@gmail.com>
2016-06-30 15:32:34 -0400

    fix escape sequences in strings.

--- utils.h
+++ utils.h
@@ -5,34 +5,34 @@
 
 
 #include <iostream>
-#include <stdio.h>
+#include <cstdio>
 #include <ctype.h>
 #include <stack>
-
+#include <cstring>
 
 extern int main();
 
 
 
 inline void displayHelp(){
-	std::cout <<"\n\t\tRPN Calculator\nPlace the operator after its two operands. \
+    std::cout <<"\n\t\tRPN Calculator\nPlace the operator after its two operands. \
 Here is an example:\n > 1 1 + \n ans = 2\n\nTo use the previous answer \
 replace one number with `ans` as in the following example:\n \
 > ans\n ans = 2\n\nWhen you are finished, type `q` or `exit` to exit the program.\n"
-			  <<std::endl;
+              <<std::endl;
 }
 
 
 CalcValue getNextValue(std::stack<CalcValue>& valStack){
-	CalcValue topVal;
-	if (!valStack.empty()) {
-		topVal = valStack.top();
-		valStack.pop();
-		return topVal;
-	} else {
-		std::cerr <<"\aERROR: not enough data to satisfy operator.\n" <<std::endl;
-		return main();
-	}
+    CalcValue topVal;
+    if (!valStack.empty()) {
+        topVal = valStack.top();
+        valStack.pop();
+        return topVal;
+    } else {
+        std::cerr <<"\aERROR: not enough data to satisfy operator.\n" <<std::endl;
+        return main();
+    }
 }
 
 // Strips backslashes from quotes
@@ -45,24 +45,28 @@ char *unescapeToken(char *token){
 
         if (in[0] == '\\' && (in[1] == '"' || in[1] == '\'')) {
             *out = in[1];
-            out++;
             in += 2;
         } else {
             *out = *in;
-            out++;
             in++;
         }
+        out++;
     }
     *out = 0;
     return token;
 }
 
+//
+static inline void deleteChar(char *word, char *toDelete){
+    memmove(toDelete, toDelete + 1, strlen(toDelete));
+}
 
 // Returns the end of the token, without changing it.
 char* qtok(char* str, char** next){
     char *current = str, *start = str;
 
-  	bool isQuoted = false;
+    bool isQuoted = false;
+    bool lastWasSlash = false;
 
     // Eat beginning whitespace.
     while (*current && isspace(*current))
@@ -77,22 +81,52 @@ char* qtok(char* str, char** next){
         // Quoted token
         start = current;
         current++; // Skip the beginning quote.
-        for (;;){
+
+
+        for (;;) {
             // Go until we find a quote or the end of string.
-            while (*current && (*current != '"'))
+            while (*current != '\0'  && *current != '"') {
+                lastWasSlash = false;
+                if (*current == '\\') {
+                    if (*(current + 1) == 'n') {
+                        *current = '\n';
+                        *(current + 1) = '\r';
+                        deleteChar(str, current + 1);
+                    } else if (*(current + 1) == 'r') {
+                        *current = *(current + 1) = '\r';
+                        current++;
+                    } else if (*(current + 1) == 't') {
+                        *current = '\t';
+                        deleteChar(str, current + 1);
+                    } else if (*(current + 1) == '\\') {
+                        *current = '\\';
+                        deleteChar(str, current + 1);
+                        lastWasSlash = true;
+                    }
+                    //current ++;
+                }
                 current++;
-            if (!*current) // Reached the end of the string.
+
+            }
+
+            //current++;
+
+
+            if (*current == '\0') // Reached the end of the string.
                 goto finalize;
 
-            if (*(current - 1) == '\\') {
+            if (*(current - 1) == '\\' && lastWasSlash == false) {
                 // Escaped quote (keep going)
                 current++;
                 continue;
             }
+
             // Reached the ending quote.
             goto finalize;
         }
     }
+
+
     // Not quoted so run till we see a space.
     while (*current && !isspace(*current))
         current++;
@@ -101,7 +135,7 @@ finalize:
 
     if (*current) {
         // Close token if not closed already.
-        *current = 0;
+        *current = '\0';
         current++;
         // Eat trailing whitespace.
         while (*current && isspace(*current))
@@ -110,20 +144,21 @@ finalize:
 
     *next = current;
 
-    return unescapeToken(start);
+    //return unescapeToken(start);
+    return start;
 
 }
 
 /*
 // Returns the end of the token, without changing it.
 char* qtok(char* str, char** next, bool& isQuoted){
-  	if (strlen(str) == 0)
-		return NULL;
+    if (strlen(str) == 0)
+        return NULL;
 
     char *current = str;
     char *start = str;
 
-  	isQuoted = false;
+    isQuoted = false;
 
     // Eat beginning whitespace.
     while (*current && isspace(*current))
