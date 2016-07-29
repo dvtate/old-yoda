df68aeb63a4af94eccbce269e40f53234623fff4
Dustin Van Tate Testa <toast27@gmail.com>
2016-07-29 16:32:55 -0400

    I like colorful error messages

---
+++ terminal_colors.h
@@ -0,0 +1,14 @@
+#ifndef TERMINAL_COLORS_H
+#define TERMINAL_COLORS_H
+
+// idk if these are winshit comptible
+#define COLOR_RESET	"\x1B[0m"
+#define COLOR_RED	"\x1B[31m"
+#define COLOR_GREEN	"\x1B[32m"
+#define COLOR_YELLOW	"\x1B[33m"
+#define COLOR_BLUE	"\x1B[34m"
+#define COLOR_MAGENTA	"\x1B[35m"
+#define COLOR_CYAN	"\x1B[36m"
+#define COLOR_WHITE	"\x1B[37m"
+
+#endif
