2a9927c6f1fe373ebd3c739a90ce95f6dd66fe4c
Dustin Van Tate Testa <toast27@gmail.com>
2016-07-29 23:26:52 -0400

    cross-platform = winshit (T.T)
    I that I have to do this..... Q^Q

---
+++ fuck_windows.h
@@ -0,0 +1,73 @@
+#ifndef FUCK_WINDOWS_H
+#define FUCK_WINDOWS_H
+
+/* An open leter to Micro$oft the authors of Win$hit
+ * WHY THE FUCK CANT WINDOWS BE POSTIX-COMPATABLE LIKE THE REST OF
+ * THE CIVILIZED WORLD, and they make me implement my own function
+ * just to make this cross-platform... SO ANGRY...
+ */
+
+#include <cstdlib>
+#include <cstdio>
+
+
+int getline(char **lineptr, size_t *n, FILE *stream) {
+
+	char *bufptr = NULL,
+		 *p = bufptr;
+
+	size_t size;
+	int c;
+
+	if (lineptr == NULL)
+		return -1;
+
+	if (stream == NULL)
+		return -1;
+
+	if (n == NULL)
+		return -1;
+
+	bufptr	= *lineptr;
+	size 	= *n;
+
+	c = fgetc(stream);
+
+	if (c == EOF)
+		return -1;
+
+	if (bufptr == NULL) {
+		bufptr = (char*) malloc(128);
+		if (bufptr == NULL)
+			return -1;
+		size = 128;
+	}
+
+	p = bufptr;
+
+	while (c != EOF) {
+		if ((p - bufptr) > (size - 1)) {
+			size = size + 128;
+			bufptr = (char*) realloc(bufptr, size);
+			if (bufptr == NULL)
+				return -1;
+		}
+		*p++ = c;
+		if (c == '\n')
+			break;
+
+		c = fgetc(stream);
+
+	}
+
+	*p++ = '\0';
+	*lineptr = bufptr;
+	*n = size;
+
+	return p - bufptr - 1;
+
+}
+
+
+
+#endif
