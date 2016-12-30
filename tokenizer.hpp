e7c57c34ae93430d207045df1c1d89d7e523632b
Dustin Van Tate Testa <toast27@gmail.com>
2016-12-30 02:31:36 -0500

    add more features to string-literal interpretation
    \nnn 
    \xhh

--- tokenizer.hpp
+++ tokenizer.hpp
@@ -9,8 +9,10 @@
 
 // removes char from string from it's pointer
 // this is extremely inefficient...
-static inline void deleteChar(char *toDelete)
+static inline void deleteChar(char* toDelete)
 	{ memmove(toDelete, toDelete + 1, strlen(toDelete)); }
+static inline void deleteChars(char* toDelete, const size_t numChars)
+	{ memmove(toDelete, toDelete + numChars, strlen(toDelete)); }
 
 // Returns the start of the token/string-constant
 // manages escape-sequences within string-constants
@@ -41,10 +43,11 @@ char* qtok(char* str, char** next){
                     if (*(current + 1) == 'n') {
                         *current = '\n';
                         *(current + 1) = '\r'; // it's not that I want to support windows...
-					       // this is just more efficienct than deleteChar()
-
+					    					   // this is just more efficienct than deleteChar()
+											   // also, this doesn't do the same thing as '\n'..
+											   // so I probably should find a better solution...
                     } else if (*(current + 1) == 'r') {
-                        *current = *(current + 1) = '\r';
+                        *current = *(current + 1) = '\r'; // nice hack :)
                         current++;
                     } else if (*(current + 1) == 't') {
                         *current = '\t';
@@ -61,18 +64,33 @@ char* qtok(char* str, char** next){
                     } else if (*(current + 1) == 'v') {
                         *current = '\v';
                         deleteChar(current + 1);
-                    } else if (*(current + 1) == '\'') {
-                        *current = '\'';
-                        deleteChar(current + 1);
-                    } else if (*(current + 1) == '\"') {
-                        *current = '\"';
-                        deleteChar(current + 1);
                     } else if (*(current + 1) == '\\') {
-                        *current = '\\';
-                        deleteChar(current + 1);
+                        deleteChar(current);
                         lastWasSlash = true;
-                    }
-                    //current++;
+                    } else if (isdigit(*(current + 1))) {
+						// escape sequence can be a maximum of 3 chars long
+                    	char str[3] = {
+                    		*(current + 1),
+                    		*(current + 2),
+                    		*(current + 3)
+                    	};
+                    	char* ptr;
+						// convert the octal literal to binary
+						*current = (char) strtol(str, &ptr, 8);
+						deleteChars(current + 1, ptr - str);
+
+                    } else if (*(current + 1) == 'x' && isdigit(*(current + 2))) {
+						// escape sequence can be a maximum of 2 chars long
+                    	char str[2] = { *(current + 2), *(current + 3) };
+                    	char* ptr;
+						// convert the octal literal to binary
+						*current = (char) strtol(str, &ptr, 16);
+						deleteChars(current + 1, ptr - str);
+
+                    } else
+						deleteChar(current);
+
+					//current++;
                 }
                 current++;
 
@@ -84,7 +102,7 @@ char* qtok(char* str, char** next){
             if (*current == '\0') // Reached the end of the string.
                 goto finalize;
 
-            if (*(current - 1) == '\\' && lastWasSlash == false) {
+            if (*(current - 1) == '\\' && !lastWasSlash) {
                 // Escaped quote (keep going)
                 current++;
                 continue;
