07b7e23be86cc96f3d5a6cd95991a8e1a8bcda5c
Dustin Van Tate Testa <toast27@gmail.com>
2017-04-14 15:51:55 -0400

    add countOccurance, str_replace; style changes

--- utils.hpp
+++ utils.hpp
@@ -71,14 +71,14 @@ char* getLineFromFile(const char* filename, size_t lineNumber){
 			fclose(file);
 			return line;
 		} else {
-			std::cerr <<"\autils.h@getLineFromFile(): line index not found";
+			std::cerr <<"\autils.h: getLineFromFile(): line index not found";
 			fclose(file);
 			return (char*) NULL;
 		}
 
 	} else {
 		std::cerr <<"\aDAFUQ: fopen(\"" <<filename <<"\", \"r\") == NULL\n"
-			  <<__FILE__ <<':' <<__LINE__ <<std::endl;
+				  <<__FILE__ <<':' <<__LINE__ <<std::endl;
 		return (char*) NULL;
 	}
 
@@ -93,23 +93,20 @@ char* getLineFromFile(FILE* file, size_t lineNumber){
 
 
 		while (getline(&line, &lineLen, file) != -1)
-			if (count == lineNumber)
-				break;
-			else
-				count++;
+			if (count == lineNumber) break;
+			else count++;
 
 		if (count) {
 			fclose(file);
 			return line;
 		} else {
-			std::cerr <<"\autils.h@getLineFromFile(): line index not found";
+			std::cerr <<"\autils.h: getLineFromFile(): line index not found";
 			fclose(file);
 			return (char*) NULL;
 		}
 
-	} else {
+	} else
 		return (char*) NULL;
-	}
 
 }
 
@@ -313,6 +310,74 @@ inline size_t linesToEnd(FILE* fil){
             ++numLines;
 
     return numLines;
+}
+
+unsigned int countOccurances(const char* str, char* sub) {
+		unsigned int ret = 0;
+		while (*str) {
+            char* substr = sub;
+            while (*substr && *substr == *str) {
+            	str++; substr++;
+			}
+            if (!*substr)
+				ret++;
+            else
+				str++;
+		}
+
+		return ret;
+}
+
+// You must free the result if result is non-NULL.
+// thanks: http://stackoverflow.com/questions/779875/what-is-the-function-to-replace-string-in-c
+char *str_replace(char *orig, char *rep, char *with) {
+    char *result; // the return string
+    char *ins;    // the next insert point
+    char *tmp;    // varies
+    int len_rep;  // length of rep (the string to remove)
+    int len_with; // length of with (the string to replace rep with)
+    int len_front; // distance between rep and end of last rep
+    int count;    // number of replacements
+
+    // sanity checks and initialization
+    if (!orig || !rep)
+        return NULL;
+
+    len_rep = strlen(rep);
+
+    // empty rep causes infinite loop during count
+    if (len_rep == 0)
+        return NULL;
+
+    if (!with)
+        with = "";
+    len_with = strlen(with);
+
+    // count the number of replacements needed
+    ins = orig;
+    for (count = 0; tmp = strstr(ins, rep); ++count) {
+        ins = tmp + len_rep;
+    }
+
+    tmp = result = (char*) malloc(strlen(orig) + (len_with - len_rep) * count + 1);
+
+    if (!result)
+        return NULL;
+
+    // first time through the loop, all the variable are set correctly
+    // from here on,
+    //    tmp points to the end of the result string
+    //    ins points to the next occurrence of rep in orig
+    //    orig points to the remainder of orig after "end of rep"
+    while (count--) {
+        ins = strstr(orig, rep);
+        len_front = ins - orig;
+        tmp = strncpy(tmp, orig, len_front) + len_front;
+        tmp = strcpy(tmp, with) + len_with;
+        orig += len_front + len_rep; // move to next "end of rep"
+    }
+    strcpy(tmp, orig);
+    return result;
 }
 
 #endif
