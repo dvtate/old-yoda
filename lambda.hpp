86e8973445c6dcaa2aef26b198b0e3c2bb3900a6
Dustin Van Tate Testa <toast27@gmail.com>
2017-06-25 01:04:48 -0400

    so there's some really shitty code associated with this...
    so there's some really shitty code associated with this, I'm probably going to have to rewrite half of it, but I would like to have this here tho

--- lambda.hpp
+++ lambda.hpp
@@ -2,6 +2,9 @@
 #define YODA_LAMBDA_HPP
 
 #include <string>
+#include <vector>
+#include <inttypes.h>
+
 #include "string_stack.hpp"
 
 // this really should just be a struct defined somewhere else...
@@ -16,6 +19,78 @@ public:
 
 	//~Lambda(){};
 
+	// are the params they gave acceptable?
+	int16_t validParams(){
+		if (!params.size())
+			return -1;
+
+		bool defaultVal = false;
+		bool va_arg = false;
+
+		for (uint8_t i = 0; i < params.size(); i++) {
+			int sps = countSpaces(i);
+			if (sps == 2) {
+				if (va_arg)
+					return i;
+				defaultVal = true;
+			} else if (sps == 1) // default arg
+				va_arg = true;
+			else if (defaultVal) // normal arg after a default val
+				return i;
+		}
+
+		return -1;
+	}
+
+	uint8_t requiredArgs(){
+		int ret = 0;
+		for (const std::string& param : params)
+			if (param.at(0) != ' ')
+				ret++;
+			else
+				return -ret;
+
+		return ret;
+	}
+
+
+	// returns a vector containing the parameter each argument will be bound to
+	// one element per argument
+	std::vector<int16_t> bindArgs(const uint8_t argc){
+		std::vector<int16_t> ret;
+		if (!validParams() || argc < requiredArgs()) {
+			ret.push_back(-1);
+			return ret;
+		}
+		//if ()
+		ret.reserve(argc);
+
+		uint8_t param = 0;
+		for (uint8_t i = 0; i < argc; i++)
+			if (param < params.size())
+				ret[i] = param++;
+			else
+				ret[i] = param;
+
+
+		return ret;
+	}
+
+
+	// this doesnt actually count spaces correctly... it conserves CPU
+	int countSpaces(int param) {
+		//if (params.size() - 1 < param)
+		//	return -2;
+		if (params[param].at(0) != ' ')
+			return 0;
+
+		for (uint16_t i = 1; i < params[param].size(); i++) {
+			if (params[param].at(i) == ' ')
+				return 2;
+		}
+
+		return 1;
+	}
 };
 
 
