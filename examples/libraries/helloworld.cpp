af3a0db58181a46f73a9a8c51fc535320fa7d75d
Dustin Van Tate Testa <toast27@gmail.com>
2018-02-04 13:50:05 -0500

    comments and stuff

--- examples/libraries/helloworld.cpp
+++ examples/libraries/helloworld.cpp
@@ -1,17 +1,22 @@
-#include "/home/tate/CLionProjects/yoda/user_defs.hpp"
+#include "user_defs.hpp"
 
 
-char* method(char* p, std::stack<CalcValue>& mainStack, std::vector<UserVar>& var_nodes,
-                 bool& showErrors, char*& rpnln, bool& elseStatement, FILE* codeFeed,
-                 std::vector<void*>& freeable) 
-{
-	std::cout <<"o shit wuddup\n";
+char* method(	char* p, 				// current token/operator
+		std::stack<CalcValue>& mainStack,	// stack (values before operator)
+		std::vector<UserVar>& var_nodes,	// variables/scopes
+		bool& showErrors,			// should errors be printed?
+		char*& rpnln,				// full line which called operator
+		bool& elseStatement,			// is there an else statement before us?
+		FILE* codeFeed,				// where we should get more code if needed
+		std::vector<void*>& freeable		// garbage collector (cleared at end of scope)
+) {
+	std::cout <<"hi there\n";
 }
 
-
+// fill vector with operators to export
 std::vector<UserDef> newOps = { UserDef().setCond("hello").setProc(method) };
 
-
+// export operators
 extern "C" {
 	void* ops_export = (void*) &newOps;
 }
